55e8f550e1fdb66c187c3e9129ad2adddac5ed3b
luis bernales <luisbernales@live.cl>
2024-06-12 17:58:32 -0400

    proyecto preprado para render

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','inmobiliaria-xzu9.onrender.com']
+CSRF_TRUSTED_ORIGINS=['http://127.0.0.1', 'https://inmobiliaria-xzu9.onrender.com']
 
 AUTH_USER_MODEL = 'web.Usuario'
 
